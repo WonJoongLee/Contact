8ee54378a6778b20f9931e0c6f8f9eade1177d32
Lee WonJoong <realjoong@gmail.com>
2021-08-21 21:25:31 +0900

    [LAYOUT] Add icon and set remember list of each value

--- app/src/main/java/com/wonjoong/android/contact/ui/DetailsScreen.kt
+++ app/src/main/java/com/wonjoong/android/contact/ui/DetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.wonjoong.android.contact.ui
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -10,9 +9,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
-import androidx.compose.runtime.collection.mutableVectorOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,18 +29,58 @@ import com.wonjoong.android.contact.data.Person
 @Composable
 fun DetailsScreen(person: Person) {
     // val getPerson = mPersonViewModel.getUserById(personId).observeAsState().value // Select Person with ID
+    var isEditing by remember { mutableStateOf(false) }
+    val inputTextList = remember {
+        mutableStateListOf(
+            person.name, // name
+            person.relationship, // relationship
+            person.age.toString(), // age
+            person.company, // company
+            person.hobby, // hobby
+            person.personality, // personality
+            person.marriage, // marriage
+            person.children, // children
+            person.like, // like
+            person.dont_like, // dontlike
+            person.etc, // etc
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
     ) {
-        Text(
-            text = person.name,
-            color = Color.Black,
-            fontSize = 28.sp,
-            modifier = Modifier
-                .padding(16.dp, 16.dp, 16.dp, 0.dp)
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = person.name,
+                color = Color.Black,
+                fontSize = 28.sp,
+                modifier = Modifier
+                    .padding(16.dp, 16.dp, 16.dp, 0.dp)
+            )
+            if (isEditing) {
+                IconButton(
+                    onClick = { isEditing = !isEditing },
+                    modifier = Modifier
+                        .padding(8.dp, 8.dp, 8.dp, 8.dp),
+                ) {
+                    Icon(imageVector = Icons.Default.Check, contentDescription = "check")
+                }
+            } else {
+                IconButton(
+                    onClick = { isEditing = !isEditing },
+                    modifier = Modifier
+                        .padding(8.dp, 8.dp, 8.dp, 8.dp),
+                ) {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = "edit")
+                }
+            }
+
+        }
         Image(
             painter = painterResource(id = R.drawable.blank_profile_pictrue),
             contentDescription = "Person Image",
@@ -50,76 +90,92 @@ fun DetailsScreen(person: Person) {
                 .padding(16.dp)
                 .clip(RoundedCornerShape(percent = 10))
         )
-        InputItem(person = person, content = "name")
-        InputItem(person = person, content = "relationship")
-        InputItem(person = person, content = "age")
-        InputItem(person = person, content = "company")
-        InputItem(person = person, content = "hobby")
-        InputItem(person = person, content = "personality")
-        InputItem(person = person, content = "marriage")
-        InputItem(person = person, content = "children")
-        InputItem(person = person, content = "like")
-        InputItem(person = person, content = "dont_like")
-        InputItem(person = person, content = "etc")
+        InputItem(person = person, content = "name", isEditing, inputTextList, 0)
+        InputItem(person = person, content = "relationship", isEditing, inputTextList, 1)
+        InputItem(person = person, content = "age", isEditing, inputTextList, 2)
+        InputItem(person = person, content = "company", isEditing, inputTextList, 3)
+        InputItem(person = person, content = "hobby", isEditing, inputTextList, 4)
+        InputItem(person = person, content = "personality", isEditing, inputTextList, 5)
+        InputItem(person = person, content = "marriage", isEditing, inputTextList, 6)
+        InputItem(person = person, content = "children", isEditing, inputTextList, 7)
+        InputItem(person = person, content = "like", isEditing, inputTextList, 8)
+        InputItem(person = person, content = "dont_like", isEditing, inputTextList, 9)
+        InputItem(person = person, content = "etc", isEditing, inputTextList, 10)
 
     }
 }
 
 @Composable
-fun InputItem(person: Person, content: String) {
+fun InputItem(
+    person: Person,
+    content: String,
+    isEditing: Boolean,
+    inputCategory: SnapshotStateList<String>,
+    index: Int
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .padding(0.dp, 8.dp),
     ) {
-
-            Text(
-                text = stringResource(id = propertyToStringValue(content)),
-                modifier = Modifier
-                    .width(132.dp)
-                    .padding(12.dp, 0.dp),
-                fontSize = 18.sp
-            )
-            var isExpanded by remember { mutableStateOf(false) }
-            val surfaceColor: Color by animateColorAsState(
-                if (isExpanded) Color.Black else Color.Gray
-            )
+        Text(
+            text = stringResource(id = propertyToStringValue(content)),
+            modifier = Modifier
+                .width(132.dp)
+                .padding(12.dp, 0.dp),
+            fontSize = 18.sp
+        )
+        var isExpanded by remember { mutableStateOf(false) }
         Surface(
             shape = MaterialTheme.shapes.medium,
             elevation = 1.dp,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(0.dp, 0.dp, 6.dp, 0.dp)
-        ){
-            Row(modifier = Modifier
-                .clickable { isExpanded = !isExpanded },
+        ) {
+            Row(
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded },
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    modifier = Modifier
-                        .animateContentSize()
-                        .weight(1f)
-                        .padding(8.dp, 0.dp),
-                    text = contentToStringValue(person, content),
-                    fontSize = 18.sp,
-                    maxLines = if (isExpanded) {
-                        Int.MAX_VALUE
-                    } else {
-                        1
-                    },
-                    color = surfaceColor,
-                    //width = Width
-                )
-                Icon(
-                    painter = if (isExpanded) {
-                        painterResource(id = R.drawable.ic_baseline_arrow_drop_up_24)
-                    } else {
-                        painterResource(id = R.drawable.ic_baseline_arrow_drop_down_24)
-                    },
-                    contentDescription = "down",
-                    Modifier.width(24.dp)
-                    //modifier = Modifier.fillMaxWidth()
-                )
+                if (isEditing) {
+//                    var tempText by remember {
+//                        mutableStateOf(
+//                            contentToStringValue(
+//                                person,
+//                                content
+//                            )
+//                        )
+//                    }
+                    //Text(text = "isEditing")
+                    TextField(
+                        value = inputCategory[index],
+                        onValueChange = { inputCategory[index] = it }
+                    )
+                } else {
+                    Text(
+                        modifier = Modifier
+                            .animateContentSize()
+                            .weight(1f)
+                            .padding(8.dp, 0.dp),
+                        text = contentToStringValue(person, content),
+                        fontSize = 18.sp,
+                        maxLines = if (isExpanded) {
+                            Int.MAX_VALUE
+                        } else {
+                            1
+                        },
+                    )
+                    Icon(
+                        painter = if (isExpanded) {
+                            painterResource(id = R.drawable.ic_baseline_arrow_drop_up_24)
+                        } else {
+                            painterResource(id = R.drawable.ic_baseline_arrow_drop_down_24)
+                        },
+                        contentDescription = "down",
+                        Modifier.width(24.dp)
+                    )
+                }
             }
         }
 
